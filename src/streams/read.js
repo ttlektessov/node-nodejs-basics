a36687f2daade9c510b68c2e31e996c19adb090a
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-04-28 22:46:07 +0500

    feat: modules read.js complete

--- src/streams/read.js
+++ src/streams/read.js
@@ -4,10 +4,10 @@ import { stdout } from "node:process";
 const read = async () => {
   const fileName = "src/streams/files/fileToRead.txt";
   const readStream = fs.createReadStream(fileName);
-  readStream.on("data", (chunk) => {
-    console.log("Chunk received:", chunk.toString());
-  });
   readStream.pipe(stdout);
+  readStream.on("end", () => {
+    console.log();
+  });
 };
 
 await read();
