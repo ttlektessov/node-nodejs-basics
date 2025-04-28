b4a40ff0dac1e1f38fb414212cece81f30937708
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-04-29 01:04:38 +0500

    refactor: refactor some paths

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,13 +1,18 @@
 import os from "os";
 import path from "path";
 import { Worker } from "node:worker_threads";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerPath = path.join(__dirname, "worker.js");
 
 const performCalculations = async () => {
   const numCPUs = os.cpus().length;
   const workers = [];
 
   for (let i = 0; i < numCPUs; i++) {
-    const worker = new Worker(path.resolve("src/wt/worker.js"), {
+    const worker = new Worker(path.resolve(workerPath), {
       workerData: 10 + i,
     });
     const workerPromise = new Promise((resolve) => {
