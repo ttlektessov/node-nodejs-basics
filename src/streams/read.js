566a8942a3d1eea6862bed0117e20f9f2ce3b761
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-04-29 01:07:33 +0500

    refactor: refactor some paths in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,9 +1,14 @@
 import fs from "node:fs";
 import { stdout } from "node:process";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const read = async () => {
-  const fileName = "src/streams/files/fileToRead.txt";
-  const readStream = fs.createReadStream(fileName);
+  const filePath = path.join(__dirname, "files", "fileToRead.txt");
+  const readStream = fs.createReadStream(filePath);
   readStream.pipe(stdout);
   readStream.on("end", () => {
     console.log();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,9 +1,14 @@
 import fs from "node:fs";
 import { stdin } from "node:process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const write = async () => {
-  const fileName = "src/streams/files/fileToWrite.txt";
-  const writeStream = fs.createWriteStream(fileName);
+  const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+  const writeStream = fs.createWriteStream(filePath);
   stdin.pipe(writeStream);
 };
 
