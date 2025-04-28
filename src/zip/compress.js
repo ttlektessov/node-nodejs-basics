206310ad6a7a46b74a22c54b84abb20177cac71f
Temirlan Tlektessov <ttlektessov@gmail.com>
2025-04-28 23:52:13 +0500

    feat: task zip:compress complete

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import process from "node:process";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream("src/zip/files/fileToCompress.txt");
+  const destination = createWriteStream("src/zip/files/archieve.gz");
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
